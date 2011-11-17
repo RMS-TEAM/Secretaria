2d26607092d0cd46312eeb97037620b61c6713a0
Sebastian <sebasvelez@gmail.com>
2011-11-17 13:56:26 -0500

    cambio en el jar

---
+++ jar/InsertarDatosGUI/src/insertardatosgui/JDBCConnection.java
@@ -0,0 +1,34 @@
+package insertardatosgui;
+
+import java.sql.DriverManager;
+import com.mysql.jdbc.Connection;
+
+public class JDBCConnection {
+
+    private static Connection connection;
+
+    public static Connection getConexion(String db, String user, String pass) throws Exception{
+        if (connection == null)
+            return getJDBConnection(db, user, pass);
+        else
+            return connection;
+    }
+
+    private static Connection getJDBConnection(String db, String user, String pass) throws Exception{
+        if(pass == null)
+            pass = "";
+        Class.forName("com.mysql.jdbc.Driver");
+        String url = "jdbc:mysql://localhost:3306/"+db+"";
+        connection = (Connection) DriverManager.getConnection(url, user, pass);
+        return connection;
+    }
+
+    public static void cerrarConexion(){
+        try{
+            connection.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+}
