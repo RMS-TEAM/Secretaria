c6b8c77db99a8ff05cb55a66931f5fb0ef0f49f0
Ruben Espinosa <rderoldan1@gmail.com>
2011-12-15 10:52:12 -0500

    cleaning directories

--- SE/app/helpers/rendimientos_helper.rb
+++
@@ -1,2 +0,0 @@
-module RendimientosHelper
-end
--- SE/test/functional/alumnos_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class AlumnosControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
