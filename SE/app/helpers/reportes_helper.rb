f3a3bd35b3d4d8c24820ac2d5de8440b88269eae
Ruben Espinosa <rderoldan1@gmail.com>
2012-03-06 12:28:06 -0500

    grades updated from the db

--- SE/app/helpers/reportes_helper.rb
+++ SE/app/helpers/reportes_helper.rb
@@ -6,5 +6,63 @@ module ReportesHelper
 
   end
 
+  def grade_ambiente(n)
+        if n <= 18.95
+          "E"
+          elsif n <= 19.64
+            "D"
+          elsif n <= 20.24
+            "C"
+          elsif n <= 20.80
+            "B"
+          else
+            "A"
+        end
+
+  end
+
+  def grade_rendimiento(n)
+    if n <= 14.22
+       "E"
+        elsif n <= 15.88
+          "D"
+        elsif n <= 16.22
+           "C"
+        elsif n <= 16.60
+           "B"
+        else
+           "A"
+     end
+  end
+
+  def grade_progreso(n)
+    if n <= 2.24
+       "E"
+        elsif n <= 6.38
+           "D"
+        elsif n <= 7.44
+           "C"
+        elsif n <= 8.70
+           "B"
+        else
+           "A"
+    end
+
+  end
+
+  def grade_total(n)
+    if n <= 42.81
+        "E"
+        elsif n <= 43.24
+           "D"
+        elsif n <= 45.08
+           "C"
+        elsif n <= 46.86
+           "B"
+        else
+           "A"
+    end
+  end
+
 
 end
