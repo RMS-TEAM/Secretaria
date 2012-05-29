659d4f4b9d1347cb0baf2053d064c0a1231832e5
Ubuntu <ubuntu@domU-12-31-38-04-89-AA.compute-1.internal>
2012-05-29 01:17:05 +0000

    nada:

--- SE/db/schema.rb
+++ SE/db/schema.rb
@@ -20,9 +20,9 @@ ActiveRecord::Schema.define(:version => 201111207160411) do
     t.datetime "updated_at"
     t.string   "password_confirmation"
     t.boolean  "admin",                 :default => false
-    t.string   "nickname"
     t.string   "encrypted_password"
     t.string   "salt"
+    t.string   "nickname"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
